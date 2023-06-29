fd618c6c197ebc2256769ccbb165c27beac4928d
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-06-29 19:58:00 +0200

    Implement try_push() for Vec<ExpressionEnum<V>>
    - Implement try_push() for Vec<ExpressionEnum<V>>.
    -Implement  EquationItem<V>.
    - Rewrite EvaluateOperator, again

--- calc/src/expression_value.rs
+++ calc/src/expression_value.rs
@@ -12,78 +12,130 @@ pub enum MathOperator {
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
-pub enum ExpressionValue<V> {
+pub enum EquationItem<V> {
     Operand(V),
     Operator(MathOperator),
 }
 
-#[derive(Debug, PartialEq)]
-pub enum RecursiveExpression<V> {
-    Value(ExpressionValue<V>),
-    Expression(Box<Layer<V>>),
+impl<V> EquationItem<V> {
+    pub fn operand(self) -> Option<V> {
+        match self {
+            EquationItem::Operand(operand) => Some(operand),
+            EquationItem::Operator(_) => None,
+        }
+    }
+    pub fn operator(self) -> Option<MathOperator> {
+        match self {
+            EquationItem::Operand(_) => None,
+            EquationItem::Operator(operator) => Some(operator),
+        }
+    }
 }
 
-pub type Layer<V> = Vec<RecursiveExpression<V>>;
+#[derive(Debug, PartialEq)]
+pub enum ExpressionEnum<V> {
+    Value(EquationItem<V>),
+    Expression(Box<Expression<V>>),
+}
+
+pub type Expression<V> = Vec<ExpressionEnum<V>>;
 
 #[derive(Debug, PartialEq)]
-pub enum EvaluateLayerError {
-    MissingOperand,
-    MissingOperator,
+pub enum ExpressionError {
+    ExpectedOperand,
+    ExpectedOperator,
     EmptyExpressionLayer,
     InvalidExpressionValueCount,
 }
 
+pub trait TryPushEpression<V> {
+    fn try_push(&mut self, item: ExpressionEnum<V>) -> Result<(), ExpressionError>;
+}
+
+impl<V> TryPushEpression<V> for Vec<ExpressionEnum<V>> {
+    fn try_push(&mut self, item: ExpressionEnum<V>) -> Result<(), ExpressionError> {
+        let last_item = self
+            .last()
+            .or(Some(&ExpressionEnum::Value(EquationItem::Operator(
+                MathOperator::Add,
+            ))))
+            .unwrap();
+
+        if matches!(item, ExpressionEnum::Value(EquationItem::Operator(_))) {
+            if matches!(last_item, &ExpressionEnum::Value(EquationItem::Operator(_))) {
+                return Err(ExpressionError::ExpectedOperand);
+            }
+        } else if matches!(last_item, &ExpressionEnum::Value(EquationItem::Operand(_)))
+            || matches!(last_item, &ExpressionEnum::Expression(_))
+        {
+            return Err(ExpressionError::ExpectedOperator);
+        }
+        self.push(item);
+        Ok(())
+    }
+}
+
+pub trait TryPushEquation<V> {
+    fn try_push(&mut self, item: EquationItem<V>) -> Result<(), ExpressionError>;
+}
+
+impl<V> TryPushEquation<V> for Vec<EquationItem<V>> {
+    fn try_push(&mut self, item: EquationItem<V>) -> Result<(), ExpressionError> {
+        let last_item = self
+            .last()
+            .or(Some(&EquationItem::Operator(MathOperator::Add)))
+            .unwrap();
+
+        match item {
+            EquationItem::Operand(_) if matches!(last_item, EquationItem::Operand(_)) => {
+                return Err(ExpressionError::ExpectedOperator)
+            }
+            EquationItem::Operator(_) if matches!(last_item, EquationItem::Operator(_)) => {
+                return Err(ExpressionError::ExpectedOperand)
+            }
+            other => {
+                self.push(other);
+                return Ok(());
+            }
+        }
+    }
+}
+
 trait EvaluateOperator {
-    fn eval_operators(
-        &self,
-        selected_operators: &[MathOperator],
-    ) -> Result<Self, EvaluateLayerError>
+    fn eval_operators(&self, selected_operators: &[MathOperator]) -> Result<Self, ExpressionError>
     where
         Self: Sized;
 }
 
-impl<V: Float + Debug> EvaluateOperator for Vec<ExpressionValue<V>> {
-    fn eval_operators(
-        &self,
-        selected_operators: &[MathOperator],
-    ) -> Result<Self, EvaluateLayerError>
+impl<V: Float + Debug> EvaluateOperator for Vec<EquationItem<V>> {
+    fn eval_operators(&self, selected_operators: &[MathOperator]) -> Result<Self, ExpressionError>
     where
         Self: Sized,
     {
-        let mut new_layer: Vec<ExpressionValue<V>> = Vec::new();
-        let mut self_iter = self.iter();
-        {
-            let first_value = match self_iter.next() {
-                Some(expression_value) => match expression_value {
-                    ExpressionValue::Operand(operand) => Ok(ExpressionValue::Operand(*operand)),
-                    ExpressionValue::Operator(_) => Err(EvaluateLayerError::MissingOperand),
-                },
-                None => Err(EvaluateLayerError::EmptyExpressionLayer),
-            }?;
-            new_layer.push(first_value);
-        }
+        let mut new_layer: Vec<EquationItem<V>> = Vec::new();
 
-        for current_equation in self_iter.as_slice().chunks(2) {
-            
-            let left_operand = match new_layer.pop() {
-                Some(ExpressionValue::Operator(_)) => Err(EvaluateLayerError::MissingOperand),
-                Some(ExpressionValue::Operand(operand)) => Ok(operand),
-                None => panic!(),
-            }?;
+        new_layer.push(*self.first().ok_or(ExpressionError::EmptyExpressionLayer)?);
 
-            let operator = match current_equation.get(0) {
-                Some(ExpressionValue::Operator(operator)) => Ok(*operator),
-                Some(ExpressionValue::Operand(_)) => Err(EvaluateLayerError::MissingOperator),
-                None => panic!(),
-            }?;
+        let self_iter = self[1..].chunks_exact(2);
 
-            let right_operand = match current_equation.get(1) {
-                Some(ExpressionValue::Operator(_)) => Err(EvaluateLayerError::MissingOperand),
-                Some(ExpressionValue::Operand(operand)) => Ok(*operand),
-                None => Err(EvaluateLayerError::MissingOperand),
-            }?;
+        for current_equation in self_iter.clone() {
+            let operator = current_equation
+                .get(0)
+                .ok_or(ExpressionError::ExpectedOperator)?
+                .operator()
+                .ok_or(ExpressionError::ExpectedOperator)?;
 
             if selected_operators.contains(&operator) {
+                let left_operand = new_layer
+                    .pop()
+                    .ok_or(ExpressionError::ExpectedOperand)?
+                    .operand()
+                    .ok_or(ExpressionError::ExpectedOperand)?;
+                let right_operand = current_equation
+                    .get(1)
+                    .ok_or(ExpressionError::ExpectedOperand)?
+                    .operand()
+                    .ok_or(ExpressionError::ExpectedOperand)?;
                 let solution = match operator {
                     MathOperator::Add => left_operand.add(right_operand),
                     MathOperator::Sub => left_operand.sub(right_operand),
@@ -93,70 +145,67 @@ impl<V: Float + Debug> EvaluateOperator for Vec<ExpressionValue<V>> {
                     MathOperator::Pow => left_operand.powf(right_operand),
                 };
                 dbg!(&solution);
-                new_layer.push(ExpressionValue::Operand(solution));
-                
+                new_layer.push(EquationItem::Operand(solution));
             } else {
                 dbg!(current_equation);
-                new_layer.push(ExpressionValue::Operand(left_operand));
-                new_layer.extend_from_slice(current_equation);
+                new_layer.try_push(
+                    *current_equation
+                        .get(0)
+                        .ok_or(ExpressionError::ExpectedOperand)?,
+                )?;
+                new_layer.try_push(
+                    *current_equation
+                        .get(1)
+                        .ok_or(ExpressionError::ExpectedOperand)?,
+                )?;
             }
         }
 
-        if (self_iter.len() - 1) % 2 == 0 {
-            match self_iter.next_back() {
-                Some(ExpressionValue::Operand(_)) => Err(EvaluateLayerError::MissingOperator),
-                Some(ExpressionValue::Operator(_)) => Err(EvaluateLayerError::MissingOperand),
-                None => unreachable!(),
-            }?;
+        if let Some(value) = self_iter.remainder().get(0) {
+            new_layer.try_push(*value)?;
         }
-
         Ok(new_layer)
     }
 }
 
-pub trait EvaluateLayer<V> {
+pub trait SolveExpression<V> {
     const ORDER_OF_OPERATIONS: [&'static [MathOperator]; 3] = [
         &[MathOperator::Pow],
         &[MathOperator::Mul, MathOperator::Div, MathOperator::Rem],
         &[MathOperator::Add, MathOperator::Sub],
     ];
 
-    fn answer(&self) -> Result<V, EvaluateLayerError>;
+    fn solve(&self) -> Result<V, ExpressionError>;
 }
 
-impl<V: Float + Debug> EvaluateLayer<V> for Layer<V> {
+impl<V: Float + Debug> SolveExpression<V> for Expression<V> {
     const ORDER_OF_OPERATIONS: [&'static [MathOperator]; 3] = [
         &[MathOperator::Pow],
         &[MathOperator::Mul, MathOperator::Div, MathOperator::Rem],
         &[MathOperator::Add, MathOperator::Sub],
     ];
 
-    fn answer(&self) -> Result<V, EvaluateLayerError> {
-        let expression_value_layer: Vec<ExpressionValue<V>> = self
+    fn solve(&self) -> Result<V, ExpressionError> {
+        let mut equation_items: Vec<EquationItem<V>> = self
             .iter()
             .map(|value_or_layer| match value_or_layer {
-                RecursiveExpression::Value(value) => Ok(*value),
-                RecursiveExpression::Expression(layer) => {
-                    layer.answer().map(|value| ExpressionValue::Operand(value))
+                ExpressionEnum::Value(value) => Ok(*value),
+                ExpressionEnum::Expression(layer) => {
+                    layer.solve().map(|value| EquationItem::Operand(value))
                 }
             })
-            .collect::<Result<Vec<ExpressionValue<V>>, EvaluateLayerError>>()?;
+            .collect::<Result<Vec<EquationItem<V>>, ExpressionError>>()?;
 
-        let mut new_layer = expression_value_layer;
         for current_order_of_operations_operators in Self::ORDER_OF_OPERATIONS.iter() {
             dbg!(&current_order_of_operations_operators);
-            new_layer = new_layer.eval_operators(&current_order_of_operations_operators)?;
-            if new_layer.len() == 1 {
-                return match new_layer
-                    .get(0)
-                    .expect("If all goes well there should be a single value")
-                {
-                    ExpressionValue::Operand(value) => Ok(*value),
-                    ExpressionValue::Operator(_) => unreachable!(),
-                };
-            }
+            equation_items = equation_items.eval_operators(&current_order_of_operations_operators)?;
         }
-        Err(EvaluateLayerError::InvalidExpressionValueCount)
+
+        equation_items
+            .get(0)
+            .ok_or(ExpressionError::EmptyExpressionLayer)?
+            .operand()
+            .ok_or(ExpressionError::ExpectedOperand)
     }
 }
 
@@ -165,25 +214,61 @@ mod evaluate_tests {
     use super::*;
 
     #[test]
-    fn eval_operators() {
-        //.into_iter().map(|v| RecursiveExpression::Value(v)).collect();
-        let test_expression: Vec<ExpressionValue<f32>> = vec![
-            ExpressionValue::Operand(1.0),
-            ExpressionValue::Operator(MathOperator::Add),
-            ExpressionValue::Operand(1.0),
-            ExpressionValue::Operator(MathOperator::Sub),
-            ExpressionValue::Operand(1.0),
+    fn eval_operators_ok() {
+        let test_expression: Vec<EquationItem<f32>> = vec![
+            EquationItem::Operand(1.0),
+            EquationItem::Operator(MathOperator::Add),
+            EquationItem::Operand(1.0),
+            EquationItem::Operator(MathOperator::Sub),
+            EquationItem::Operand(1.0),
         ];
-        let test_expression_answer: Vec<ExpressionValue<f32>> = vec![
-            ExpressionValue::Operand(2.0),
-            ExpressionValue::Operator(MathOperator::Sub),
-            ExpressionValue::Operand(1.0),
+        let test_expression_answer: Vec<EquationItem<f32>> = vec![
+            EquationItem::Operand(2.0),
+            EquationItem::Operator(MathOperator::Sub),
+            EquationItem::Operand(1.0),
         ];
         assert_eq!(
             test_expression
                 .eval_operators(&[MathOperator::Add])
                 .unwrap(),
             test_expression_answer
+        );
+    }
+    
+    #[test]
+    fn eval_operators_err() {
+
+    }
+
+    #[test]
+    fn try_push() {
+        let mut test_expression: Vec<ExpressionEnum<f32>> = vec![];
+        assert_eq!(
+            test_expression
+                .try_push(ExpressionEnum::Value(EquationItem::Operator(
+                    MathOperator::Add
+                )))
+                .unwrap_err(),
+            ExpressionError::ExpectedOperand
+        );
+        assert_eq!(test_expression.last(), None);
+        assert!(test_expression
+            .try_push(ExpressionEnum::Value(EquationItem::Operand(5.0)))
+            .is_ok());
+        assert_eq!(
+            test_expression.last(),
+            Some(&ExpressionEnum::Value(EquationItem::Operand(5.0f32)))
+        );
+        assert!(test_expression
+            .try_push(ExpressionEnum::Value(EquationItem::Operator(
+                MathOperator::Add
+            )))
+            .is_ok());
+        assert_eq!(
+            test_expression.last(),
+            Some(&ExpressionEnum::Value(EquationItem::Operator(
+                MathOperator::Add
+            )))
         );
     }
 }
--- calc/src/main.rs
+++ calc/src/main.rs
@@ -3,9 +3,9 @@ use expression_value::*;
 
 fn main() {
     println!("Hello, world!");
-    let layer_one = Box::new(vec![RecursiveExpression::<f32>::Value(ExpressionValue::Operand(1.0)), RecursiveExpression::<f32>::Value(ExpressionValue::Operator(MathOperator::Add)), RecursiveExpression::<f32>::Value(ExpressionValue::Operand(1.0))]);
-    let mut layer_two: Layer<f32> = vec![RecursiveExpression::<f32>::Value(ExpressionValue::Operand(1.0)), RecursiveExpression::<f32>::Value(ExpressionValue::Operator(MathOperator::Add)), RecursiveExpression::<f32>::Expression(layer_one)];
-    dbg!(&layer_two); 
+    let layer_one = Box::new(vec![ExpressionEnum::<f32>::Value(EquationItem::Operand(1.0)), ExpressionEnum::<f32>::Value(EquationItem::Operator(MathOperator::Add)), ExpressionEnum::<f32>::Value(EquationItem::Operand(1.0))]);
+    let mut layer_two: Expression<f32> = vec![ExpressionEnum::<f32>::Value(EquationItem::Operand(1.0)), ExpressionEnum::<f32>::Value(EquationItem::Operator(MathOperator::Add)), ExpressionEnum::<f32>::Expression(layer_one)];
+    dbg!(5 % 2); 
 
     let test = vec![0;10];
 
