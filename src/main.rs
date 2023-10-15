58c58c9853c87f5adce1c8cd066c019518e8368b
JokNavi <JokNavi1@gmail.com>
2023-10-15 13:17:38 +0200

    Add working main file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
+use clap::{Command, arg, ArgMatches};
+
+use crate::eval::Evaluate;
+const VERSION: &str = "1.0.0";
+
 pub mod operator;
 pub mod expression_item;
 pub mod eval;
 pub mod expression_builder;
 
+fn get_cmd() -> ArgMatches {
+    Command::new("calc").about("Calculator").args(&[
+        arg!(-v --version "Prints the version number"),
+        arg!(-e --equation <String> "Equation")
+    ]).get_matches()
+} 
+
 fn main() {
-    println!("Hello, world!");
+    let matches = get_cmd();
+    if matches.get_one::<bool>("version").is_some_and(|bool| *bool) {
+        println!("{}", VERSION);
+    };
+    if let Some(equation) = matches.get_one::<String>("equation") {
+        match equation.as_str().eval() {
+            Ok(result) => println!("{}", result.unwrap()),
+            Err(err) => println!("{}", err)
+        }
+    }
 }
